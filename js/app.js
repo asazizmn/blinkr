2944e72af1ee1ae52e0ca10ecd007c518622226f
Aziz Saboor <azizsaboor@gmail.com>
2016-02-28 11:22:11 +0800

    - retrieving a list of masjid photos instead

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var main = function () {
     "use strict";  
     
-    var flickr_req = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?';
+    var flickr_req = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=masjids&format=json&jsoncallback=?';
     $.getJSON(flickr_req, function (flickr_res) {
          flickr_res.items.forEach(function (photo) {
              var photo_url = photo.media.m;
