573f2cd5d43ce23f1f0836c33158d53b5489475d
Aziz Saboor <azizsaboor@gmail.com>
2016-02-28 11:12:26 +0800

    - pulling images from flickr and appending them to index main

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 var main = function () {
     "use strict";  
     
-    console.log('hello world');
+    var flickr_req = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?';
+    $.getJSON(flickr_req, function (flickr_res) {
+         flickr_res.items.forEach(function (photo) {
+             var photo_url = photo.media.m;
+             var $photo = $('<img>').attr('src', photo_url);
+             
+             $('#flickr_imgs').append($photo);
+         });
+    });
 };
 
 $(document).ready(main);
