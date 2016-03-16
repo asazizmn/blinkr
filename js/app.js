7f484a0e523ad7915ce48c058b6037d466ac9fd0
Aziz Saboor <azizsaboor@gmail.com>
2016-03-16 18:01:17 +0800

    - did front end revamp - disabled flickr api request temporarily

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 var main = function () {
     "use strict";  
     
-    var flickr_req = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=masjids&format=json&jsoncallback=?';
+    /*var flickr_req = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=masjids&format=json&jsoncallback=?';
     $.getJSON(flickr_req, function (flickr_res) {
          flickr_res.items.forEach(function (photo) {
              var photo_url = photo.media.m;
              var $photo = $('<img>').hide();
              $photo.attr('src', photo_url);
              
-             $('#flickr_imgs').append($photo);
+             $('#flickr-imgs').append($photo);
              $photo.fadeIn('slow');
          });
-    });
+    });*/
 };
 
 $(document).ready(main);
