538a0840a20c1f40c9cdce60e75e5e3346891c89
Aziz Saboor <azizsaboor@gmail.com>
2016-03-22 14:29:52 +0800

    - now searching for imgs & displaying all at once - changed footer opacity

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
 var main = function () {
     "use strict";  
     
-    /*var flickr_req = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=masjids&format=json&jsoncallback=?';
-    $.getJSON(flickr_req, function (flickr_res) {
-         flickr_res.items.forEach(function (photo) {
-             var photo_url = photo.media.m;
-             var $photo = $('<img>').hide();
-             $photo.attr('src', photo_url);
-             
-             $('#flickr-imgs').append($photo);
-             $photo.fadeIn('slow');
-         });
-    });*/
+    var $tagSearchField = $('#tag-search'), tagSearchTerm;
+    var flickrReqBase = 'http://api.flickr.com/services/feeds/photos_public.gne', flickrReq;
+    
+    $tagSearchField.on('keypress', function (event) {
+        
+        // check for enter key
+        if (event.keyCode === 13) {
+            tagSearchTerm = $tagSearchField.val();
+            
+            // build request URL
+            if (tagSearchTerm !== '') {
+                flickrReq = flickrReqBase + '?tags=' + tagSearchTerm + '&format=json&jsoncallback=?';
+                
+                // request images
+                $.getJSON(flickrReq, function (flickrRes) {
+                    $('#flickr-img').empty();
+                    flickrRes.items.forEach(function (photo) {
+                        var photoURL = photo.media.m;
+                        var $photo = $('<img>').hide();
+                        $photo.attr('src', photoURL);
+
+                        $('#flickr-img').append($photo);
+                        $photo.fadeIn('slow');
+                    });
+                });
+            }
+        }
+    });
 };
 
 $(document).ready(main);
